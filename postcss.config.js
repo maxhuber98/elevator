8a09ca638f3a516c296cacd280d03f76a7315ce8
Max huber <max.huber98@hotmail.com>
2020-04-10 20:06:47 +0200

    fixed image path not found

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    cssnano: {},
+    autoprefixer: {},
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,17 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
   output: {
     filename: '[name].bundle.js',
-    publicPath: '/',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   module: {
     rules: [
-      {
-        test: /\.(png|jpe?g\gif)$/i,
-        loader: 'url-loader',
-      },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -26,11 +22,20 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['dist'] }),
+    // new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['dist'] }),
     new HtmlWebpackPlugin({ template: './index.html' }),
+    new CopyPlugin([{ from: './src/assets/images', to: 'images' }]),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
